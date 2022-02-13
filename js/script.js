c84a9bb82c9204fa6f4b4811b5a4bdb9106165ce
morodiasalexander07 <morodiasalexander@gmail.com>
2022-02-13 05:21:53 -0400

    revision

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 let player = document.getElementById('player');
 let mainBody = document.getElementById('mainBody');
 let loadingPage = document.getElementById('loading');
+let textHidden = document.getElementById('textHidden');
+let flork = document.getElementById('flork');
+let textVisible = document.getElementById('textVisible');
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let buttonsContainer = document.getElementById('buttonsContainer');
+
 
 function volumen() {
     player.volume = '.2';
@@ -20,3 +27,17 @@ function EliminarPantallaDeCarga() {
 }
 
 setTimeout(EliminarPantallaDeCarga, 3600);
+
+function success() {
+
+    button1.style.display = 'none';
+    button1.style.opacity = '0';
+    button2.style.display = 'none';
+    button2.style.opacity = '0';
+
+    textHidden.style.opacity = '1';
+    flork.src = "img/passed.png";
+    flork.style.width = "350px";
+    flork.style.height = "350px";
+    textVisible.style.opacity = '0';
+}
