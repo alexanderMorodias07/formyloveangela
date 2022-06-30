8a0055b43141e85a66ea84f2a725deff023d8572
Alexander Morodias <60762242+AlexanderMorodias@users.noreply.github.com>
2022-06-29 20:46:38 -0400

    cumpleaños
    contador para el cumpleaños

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import CountDown from "./cuentaRegresiva.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) =>{
+    CountDown("countDown", "jul 3, 2022 00:00:00", "Feliz Cumpleaños");
+})
