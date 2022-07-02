7ff62e8734e6d0ba2187c9b77673346d3821271c
Alexander Morodias <99577269+alexanderMorodias07@users.noreply.github.com>
2022-07-02 19:44:35 -0400

    .
    .

--- js/cuentaRegresiva.js
+++ js/cuentaRegresiva.js
@@ -23,8 +23,8 @@ export default function CountDown(id, limitDate, finalMessage){
             // $contDown.innerHTML = `<p class="Time">000:00:00:00</p>`;
             // CountDown("countDown", "Jul 3, 2023 00:00:00", "Feliz Cumpleaños");
             // $wait.innerHTML = `Para el proximo Cumpleaños`;
-            d.getElementById("modalBtn").classList.add("modal-btn");
-            d.getElementById("modalBtn").innerHTML = `Click`;
+            // d.getElementById("modalBtn").classList.add("modal-btn");
+            // d.getElementById("modalBtn").innerHTML = `Click`;
             
         }   
     }, 1000)
