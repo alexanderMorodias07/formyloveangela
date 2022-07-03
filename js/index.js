729ab8902e1d9098745f079f93d3c18802df48e9
Alexander Morodias <99577269+alexanderMorodias07@users.noreply.github.com>
2022-07-02 20:11:30 -0400

    .
    .

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import CountDown from "./cuentaRegresiva.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", () =>{
-   CountDown("countDown", "Jul 3, 2022 20:10:00", "Feliz Cumpleaños");
+   CountDown("countDown", "Jul 3, 2022 20:15:00", "Feliz Cumpleaños");
    generar(".modal-btn", ".first-view")
    //Contador("contadorAge", 22);
 })
